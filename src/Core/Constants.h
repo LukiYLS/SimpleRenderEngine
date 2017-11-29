f368f84978e06c0894b4de2c927fec8a48d67346
Luki <897847201@qq.com>
2017-11-29 17:44:59 +0800

    update

--- src/Core/Constants.h
+++ src/Core/Constants.h
@@ -51,6 +51,18 @@ namespace SRE {
 	
 	const char* vertex_attribute = "layout(location=0)in vec3 position;\nlayout(location =1) in vec3 normal;\nlayout(location=2)in vec3 color;\nlayout(location=3)in vec2 texCoord;\nuniform mat4 modelMatrix;\nuniform mat4 viewMatrix;\nuniform mat4 projectionMatrix;\n";
 
-	const char* uv_vertex = "#if (define USE_MAP) || (define USE_NORMAL_MAP)\nvaying vec2 uv;\nuniform vec3 uvTransform;\n#endif";
+	const char* uv_vertex_define = "#if define (USE_MAP) || define (USE_NORMAL_MAP)\nvaying vec2 uv;\nuniform vec3 uvTransform;\n#endif";
+
+	const char* uv2_vertex_define = "#if define(USE_LIGHT_MAP) || define(USE_ AOMAP)\nvarying vec2 uv2;\n#endif";
+
+	const char* envmap_vertex_define = "#ifdef USE_ENVMAP\n#if defined(USE_BUMPMAP) || defined(USE_NORMALMAP) || defined(PHONG)\n\tvarying vec3 worldPosition;\n#else\n\tvarying vec3 reflect;\n\tuniform float refractionRatio;\n\t#endif\n#endif\n";
+
+	const char* color_vertex_define = "#ifdef USE_COLOR\n\tvarying vec3 Color;\n#endif\n";
+
+	const char* uv_vertex = "#if define (USE_MAP) || define (USE_NORMAL_MAP)\nuv = (uvTransform * vec3(uv, 1)).xy;\n#endif";
+
+	const char* uv2_vertex = "#if define(USE_LIGHT_MAP) || define(USE_ AOMAP)\nuv2 = uv2;\n#endif";
+
+	const char* color_vertex = "#ifdef USE_COLOR\n\tColor = color;\n#endif\n";
 
 }
